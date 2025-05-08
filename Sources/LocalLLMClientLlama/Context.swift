4cde020f4a79be44534dd736a72ff6e2afaa04ae
tattn (Tatsuya Tanaka) <contact@tattn.dev>
2025-05-09 03:00:55 +0900

    refactor: restructure Context and Model classes for improved clarity and functionality

--- Sources/LocalLLMClientLlama/Context.swift
+++ Sources/LocalLLMClientLlama/Context.swift
@@ -9,11 +9,11 @@ public final class Context {
     var sampling: Sampler
     let grammer: Sampler?
     var cursor: [llama_token_data]
-    private let model: OpaquePointer
+    let model: Model
     let extraEOSTokens: Set<String>
 
     package var vocab: OpaquePointer {
-        llama_model_get_vocab(model)
+        model.vocab
     }
 
     package var addBos: Bool {
@@ -27,28 +27,14 @@ public final class Context {
     public init(url: URL, parameter: LlamaClient.Parameter = .default) throws(LLMError) {
         initializeLlama()
 
-        var model_params = llama_model_default_params()
-#if targetEnvironment(simulator)
-        model_params.n_gpu_layers = 0
-#endif
-        model_params.use_mmap = true
-
-        guard let model = llama_model_load_from_file(url.path(), model_params) else {
-            throw .failedToLoad(reason: "Failed to load model from file")
-        }
-
         var ctx_params = llama_context_default_params()
         ctx_params.n_ctx = UInt32(parameter.context)
         ctx_params.n_threads = Int32(parameter.numberOfThreads ?? max(1, min(8, ProcessInfo.processInfo.processorCount - 2)))
         ctx_params.n_threads_batch = ctx_params.n_threads
 
-        guard let context = llama_init_from_model(model, ctx_params) else {
-            throw .invalidParameter
-        }
-
         self.parameter = parameter
-        self.model = model
-        self.context = context
+        self.model = try Model(url: url)
+        self.context = try model.makeAndAllocateContext(with: ctx_params)
         batch = llama_batch_init(Int32(parameter.batch), 0, 1)
         extraEOSTokens = parameter.options.extraEOSTokens
 
@@ -62,7 +48,7 @@ public final class Context {
         llama_sampler_chain_add(sampling, llama_sampler_init_typical(parameter.typicalP, minKeep))
         llama_sampler_chain_add(sampling, llama_sampler_init_penalties(Int32(parameter.penaltyLastN), parameter.penaltyRepeat, 0, 0))
 
-        let cursorCount = Int(llama_vocab_n_tokens(llama_model_get_vocab(model)))
+        let cursorCount = Int(llama_vocab_n_tokens(model.vocab))
         cursor = Array(unsafeUninitializedCapacity: cursorCount) { _, initializedCount in
             initializedCount = cursorCount
         }
@@ -72,12 +58,12 @@ public final class Context {
             case .json:
                 do {
                     let template = try String(contentsOf: Bundle.module.url(forResource: "json", withExtension: "gbnf")!, encoding: .utf8)
-                    grammer = llama_sampler_init_grammar(llama_model_get_vocab(model), template, "root")
+                    grammer = llama_sampler_init_grammar(model.vocab, template, "root")
                 } catch {
                     throw .failedToLoad(reason: "Failed to load grammar template")
                 }
             case let .grammar(grammar, root):
-                grammer = llama_sampler_init_grammar(llama_model_get_vocab(model), grammar, root)
+                grammer = llama_sampler_init_grammar(model.vocab, grammar, root)
             }
             llama_sampler_chain_add(sampling, grammer)
         } else {
@@ -88,7 +74,6 @@ public final class Context {
     deinit {
         llama_sampler_free(sampling)
         llama_batch_free(batch)
-        llama_model_free(model)
         llama_free(context)
     }
 }
---
+++ Sources/LocalLLMClientLlama/Model.swift
@@ -0,0 +1,104 @@
+import Foundation
+import LocalLLMClient
+import Jinja
+
+final class Model {
+    let model: OpaquePointer
+    let chatTemplate: String?
+
+    var vocab: OpaquePointer {
+        llama_model_get_vocab(model)
+    }
+
+    init(url: URL) throws(LLMError) {
+        var model_params = llama_model_default_params()
+#if targetEnvironment(simulator)
+        model_params.n_gpu_layers = 0
+#endif
+        model_params.use_mmap = true
+
+        guard let model = llama_model_load_from_file(url.path(), model_params) else {
+            throw .failedToLoad(reason: "Failed to load model from file")
+        }
+
+        self.model = model
+
+        let chatTemplate = getString(capacity: 1024) { buffer, length in
+            // LLM_KV_TOKENIZER_CHAT_TEMPLATE
+            llama_model_meta_val_str(model, "tokenizer.chat_template", buffer, length)
+        }
+        self.chatTemplate = chatTemplate.isEmpty ? nil : chatTemplate
+    }
+
+    deinit {
+        llama_model_free(model)
+    }
+
+    func makeAndAllocateContext(with ctx_params: llama_context_params) throws(LLMError) -> OpaquePointer {
+        guard let context = llama_init_from_model(model, ctx_params) else {
+            throw .invalidParameter
+        }
+        return context
+    }
+
+    func tokenizerConfigs() -> [String: Any] {
+        let numberOfConfigs = llama_model_meta_count(model)
+        return (0..<numberOfConfigs).reduce(into: [:]) { partialResult, i in
+            let key = getString(capacity: 64) { buffer, length in
+                llama_model_meta_key_by_index(model, i, buffer, length)
+            }
+            let value = getString(capacity: 1024) { buffer, length in
+                llama_model_meta_val_str_by_index(model, i, buffer, length)
+            }
+            partialResult[key] = value
+        }
+    }
+
+    func applyTemplate(_ messages: [[String: Any]], additionalContext: [String: Any]? = nil) throws(LLMError) -> String {
+        guard let chatTemplate else {
+            throw .failedToLoad(reason: "Failed to load template")
+        }
+        do {
+            let template = try Template(chatTemplate)
+
+            var context: [String: Any] = [
+                "messages": messages,
+                "add_generation_prompt": true,
+            ]
+            //    if let tools {
+            //        context["tools"] = tools
+            //    }
+            if let additionalContext {
+                context.merge(additionalContext) { _, new in new }
+            }
+
+            let specialTokenAttributes: [String] = [
+                "bos_token",
+                "eos_token",
+                "unk_token",
+                "sep_token",
+                "pad_token",
+                "cls_token",
+                "mask_token",
+                "additional_special_tokens",
+            ]
+
+            for (key, value) in tokenizerConfigs() where specialTokenAttributes.contains(key) {
+                context[key] = value
+            }
+
+            return try template.render(context)
+        } catch {
+            throw .invalidParameter
+        }
+    }
+}
+
+private func getString(capacity: Int = 1024, getter: (UnsafeMutablePointer<CChar>?, Int) -> Int32) -> String {
+    String(unsafeUninitializedCapacity: capacity) { buffer in
+        buffer.withMemoryRebound(to: CChar.self) { buffer in
+            let length = Int(getter(buffer.baseAddress, capacity))
+            return max(0, length)
+        }
+    }
+}
